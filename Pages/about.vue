6465636b25137c46ee2f15fbf5fa92e53371179f
sarthaksavvy <sarthak@bitfumes.com>
2022-09-04 00:37:55 +0530

    09 Nuxtjs course 2022 - Page Navigation

--- Pages/about.vue
+++ Pages/about.vue
@@ -1,1 +1,7 @@
-<template>Hello About</template>
+<template>
+  <div>
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="about">About</NuxtLink>
+    Hello About
+  </div>
+</template>
--- Pages/index.vue
+++ Pages/index.vue
@@ -1,1 +1,15 @@
-<template>Hello Index</template>
+<template>
+  <div>
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="about">About</NuxtLink>
+
+    <button @click="moveToAbout">Take me to About Page</button>
+    Hello Index
+  </div>
+</template>
+
+<script setup>
+function moveToAbout() {
+  navigateTo("about");
+}
+</script>
