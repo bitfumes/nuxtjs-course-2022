bc31dab2a838894d4941f62067500a4550af701b
sarthaksavvy <sarthak@bitfumes.com>
2022-09-03 13:05:33 +0530

    05 Nuxtjs course 2022 - auto import

--- app.vue
+++ app.vue
@@ -1,3 +1,18 @@
 <template>
-  <div>Hello World</div>
+  <div>Hello Worldq</div>
+  <button @click="increment">Increment</button>
+  <p>{{ count }}</p>
+  <button @click="decrement">Decrement</button>
 </template>
+
+<script>
+const count = ref(0);
+
+function increment() {
+  count.value++;
+}
+
+function decrement() {
+  count.value--;
+}
+</script>
