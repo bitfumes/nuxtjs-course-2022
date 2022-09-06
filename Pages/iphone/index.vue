add0da6ee2f72cf7fcc321b1abe4856af508231d
sarthaksavvy <sarthak@bitfumes.com>
2022-09-06 11:21:54 +0530

    13 Nuxtjs course 2022 - public directory

---
+++ Pages/iphone/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div>
+      <h1>IPhone 12</h1>
+      <img width="200" src="/images/iphone12.webp" alt="" />
+    </div>
+    <div>
+      <h1>IPhone 12 Pro</h1>
+      <img width="200" src="/images/iphone12pro.jpg" alt="" />
+    </div>
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
-    <nav class="bg-purple-700 text-white">
+    <nav
+      class="bg-purple-700 shadow-lg p-3 text-white text-lg flex justify-between"
+    >
       <NuxtLink to="/">Home</NuxtLink>
-      <NuxtLink to="about">About</NuxtLink>
-      <NuxtLink to="profile">Profile</NuxtLink>
+      <NuxtLink to="/about">About</NuxtLink>
+      <NuxtLink to="/iphone">Iphones</NuxtLink>
+      <NuxtLink to="/profile">Profile</NuxtLink>
     </nav>
 
     <!-- here nuxt will replace page content -->
